7adba4a1afefb9bda6239ce7ff328714eb1f4173
zenfirassh-bpa203-glitch <zenfirassh-bpa203@code.edu.az>
2025-10-19 14:54:08 +0400

    Program

---
+++ 08.Object, Class, Constructor, Inheritance, this vs base keywords/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+public class  Person 
+{
+    public string FirstName;
+    public string LastName;
+    public int Age;
+    public string Email;
+    public string Id;
+
+
+    public Person(string firstName, string lastName, int age, string email, string id)
+    {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Age = age;
+        this. Email = email;
+        this.Id = id;
+    }
+
+    public string GetFullName()
+    {
+        return $"{FirstName} {LastName}";
+    }
+
+    public virtual void ShowBasicInfo() {
+
+        Console.WriteLine($"ID: {Id}");
+        Console.WriteLine($"Full Name: {GetFullName()}");
+        Console.WriteLine($"Age: {Age}");
+        Console.WriteLine($"Email: {Email}");
+    }
+
+
+
+
+
+}
