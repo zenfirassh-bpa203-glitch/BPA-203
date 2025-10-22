81143a72c9b8b8097dd89f09dc3596c5a1a91ba0
zenfirassh-bpa203-glitch <zenfirassh-bpa203@code.edu.az>
2025-10-22 11:19:56 +0400

    program

---
+++ 10.Access Modifires, Encupsulation, Namespace/GET.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Access_Modifires__Encupsulation__Namespace2
+{
+    internal class GET
+    {
+        public int HorsePower
+        {
+            get
+            {
+                return HorsePower;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    HorsePower = 0;
+                }
+                else
+                {
+                    HorsePower = value;
+                }
+            }
+        }
+    }
+}
---
+++ 10.Access Modifires, Encupsulation, Namespace/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Access_Modifires__Encupsulation__Namespace2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GET get = new GET();
+            get.HorsePower = 500;
+            Console.WriteLine(get.HorsePower);
+        }
+    }
+}
